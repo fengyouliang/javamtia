395fdada64f171bf6785041ba909dc166732e7c8
youliang <1654388696@qq.com>
2022-08-14 11:07:39 +0800

    Initial commit

---
+++ src/test/java/com/example/javamtia/JavamtiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javamtia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavamtiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
